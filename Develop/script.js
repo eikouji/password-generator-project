3e5f463b182a14e5c4216e95b23ac41cb12e3546
Eiko ujifusa <eikorouji@gmail.com>
2021-12-04 23:31:14 -0800

    trying a number of strategies with this project. Not giving up yet.

--- Develop/script.js
+++ Develop/script.js
@@ -1,32 +1,3 @@
-// Assignment code here
-var generateBtn = document.querySelector("#generate");
-
-// define generatePassword with a function ////////
-  function generatePassword() {
-    // a console log to check that the button is working ////////
-    console.log("You've clicked this button! It's working beep boop.");
-
-
-// parameters for password generation //////////////////////////////////
-  
-
-//Certain number of characters in password. More than 8, no more than 28 //////////////////////////////////
- var characterNumber =
-
-
-// Generate Random Upper Case Letters //////////////////////////////////
- var randomUpperCaseLetter = 
-
-// Generate Random Lower Case Letters // 
- var randomLowerCaseLetter =
-
-// Generate Symbol to improve security/guess-ability //////////////////////////////////
- var randomSymbol =
-
-// console log the result of the password, what it should look like when the right password is generated ////////////
-//console.log(characterNumber + randomUppercaseLetter + randomLowerCaseLetter + randomSymbol);
-
-
 
 // Acceptance Needs To Be Done //////////////////////////////////
 // 1. Prompt the User for the password criteria
@@ -35,11 +6,158 @@ var generateBtn = document.querySelector("#generate");
 // 2. Validate Input //
 
 
-// 3. Display password to the page, which is the return ! // 
-    return "Generated password will go here! Beep Boop";
-  }
-  
+// Assignment code here // 
 
+// Define generatePassword with a function ////////
+  function generatePassword () {
+    // a console log to check that the button is working ////////
+    console.log('Generating password with ' + charactersLength + randomUppercaseLetter + randomLowerCaseLetter + randomSymbol + '. ');
+
+  // Establish character amount for passwords, between 8 - 128 // //
+  var characterAmount = passwordCharacterAmount();
+
+
+  // Completed Password, with ALL password rules met ////////
+  var completedPassword = comPletedPassword();
+
+ // Generate letters, upper and lower case // //
+    var letters = "ABCDEFGHJKLMNOPQRSTUVWXYZabcdefghijlkmnopqrstuvwxyz";
+
+    var letter = letters [0]; // "A"
+    var randomLetter = letters[Math.floor(Math.random() * characters.length)]; // random letter ;)
+
+ // Generate Random Number to put in with upper case and lower case letters // //
+ var randomNumber = function() {
+   Math.floor(Math.random() * 98);
+ }
+
+ // Generate a Special Symbol to improve security/guess-ability //////////////////////////////////
+ var specialSymbol = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "-", ".", "`", "~", "|", "<", ">", "=", "-", "_"];
+
+
+
+// Randomized selection of indicated password rules // //
+ var passwordText = "";
+ var selection = 0;
+ for (var i = 0; i < charactersLength; i++) {
+   // keep running this until landing on one of the selected rules // //
+   var selected = false;
+   while (!selected) {
+     var characterType = Math.floor(Math.random() * 4);
+     if (completedPassword.includes(characterType)) {
+      switch (characterType) {
+        case w:
+          // random number function example: function getRndInteger(min, max) {
+          // return Math.floor(Math.random() * (max - min + 1) ) + min;
+         
+          selection = getRndInteger(8, 128) {
+              return Math.floor(Math.random() * (128 - 8) + 1) ) + min;
+      }
+  
+        case x: 
+          selection = Math.floor(Math.random() * specialCharacters.length); 
+          passwordText += String.fromCharCode(selection);
+          break;
+  
+        default:
+          break;
+      }
+      selected = true;
+     }
+   }
+
+  }
+   return passwordText;
+
+  };
+
+
+
+  /* Switch example:
+   switch(expression) {
+  case x:
+    // code block
+    break;
+  case y:
+    // code block
+    break;
+  default:
+    // code block
+
+*/
+
+
+
+// get password with p-word rules // 
+  var getPasswordCompleted = function () {
+        // make sure user has all password rules // //
+        var confirmUpperCase = confirm("Please include uppercase letters");
+        var confirmLowerCase = confirm("Please include lowercase letters");
+        var confirmNumbers = confirm("Indicate if you would like numbers in your password");
+        var confirmSpecial = confirm("Indicate if you would like special characters like !@#$% in your password");
+
+    // Make a list with a number for each rule select (numbers, lower, upper, special) //
+
+    var passwordCompleted = [];
+    if (confirmUpperCase) {
+      passwordCriteria.push(0);
+    }
+    if (confirmLowerCase) {
+      passwordCriteria.push(1);
+    }
+    if (confirmNumbers) {
+      passwordCriteria.push(2);
+    }
+    if (confirmSpecial) {
+      passwordCriteria.push(3);
+    }
+
+    // re-do password criteria check if no rules are selected // 
+    if (passwordCompleted.length === 0) {
+      alert("Error: Your password must have at least one rule selected");
+        return getPasswordCriteria();
+    }
+   
+    return passwordCompleted;
+  }
+
+
+ // Randomize parameters of the password, randomize character selection // //
+   var passwordText = "";
+   var selection = 0();
+   for (i = 0; i < passwordCriteria.length; i++) {
+
+    // Generate random number to act as password character length // //
+
+  };
+
+  var getPasswordLength = function () {
+    // Ask user to make a password with required characters amount // //
+    var passwordLength = parsInt(
+      prompt("Enter a password with 8 or more characters, max 128 in length")
+      );
+
+    // if the password is has too few or too many characters, warn user with a prompt // //
+    if(passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
+      alert("Error! You must have a password with at least 8 characters or more; 128 characters max");
+
+      return getPasswordLength();
+    }
+
+    // acceptable password length // //
+    return passwordLength;
+
+  };
+
+
+// Display password to the page, which is the return ! // 
+    return passwordCriteria;
+    // a test console.log // 
+    console.log("Generated password will go here! Beep Boop");
+  };
+  
+// #generate element // //
+ var generateBtn = document.querySelector("#generate");
 
 
 // Write password to the #password input
@@ -47,12 +165,10 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-
   passwordText.value = password;
 
 }
 
-// Add event listener to generate button
+// Add event listener to generate button //
 generateBtn.addEventListener("click", writePassword);
-
 
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# Password Generator Starter Code
+
+
+# Password Generator Project #
+
+A project to practice JavaScript to create a strong password.
+This password has:
+1. Upper case letters
+2. Lower case Letters
+3. A random symbol like !@#$%^&*()
+4. Has a character minimum of 8 characters long
+
+
+# Screencap of the project
+
+# Link of the GitHub Page
+
+
